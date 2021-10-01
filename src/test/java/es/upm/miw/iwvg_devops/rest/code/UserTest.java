458874e80d790f2b2246153cdb4cd80fcdd91e41
Mariavernia <maversa2@alumni.uv.es>
2021-10-01 10:40:03 +0200

    create user class and content and add user test class #7

---
+++ src/test/java/es/upm/miw/iwvg_devops/rest/code/UserTest.java
@@ -0,0 +1,15 @@
+package es.upm.miw.iwvg_devops.rest.code;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UserTest {
+
+    @Test
+    void findAllUsers() {
+
+    }
+}
