16ecb95dc88cdd886cb671803c2ad95ea4c82e1e
Mariavernia <maversa2@alumni.uv.es>
2021-10-01 18:35:39 +0200

    Fix code smell and improve test coverage #15

--- src/test/java/es/upm/miw/iwvg_devops/rest/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/code/FractionTest.java
@@ -10,12 +10,15 @@ import java.util.List;
 
 class FractionTest {
     private User user;
+
     @BeforeEach
     void before() {
         List<Fraction> fractions1 = List.of(
                 new Fraction(0, 4),
                 new Fraction(1, -3),
-                new Fraction(3, 1)
+                new Fraction(4, 2),
+                new Fraction(2, 1),
+                new Fraction(7, 2)
         );
         user = new User("0", "Maria", "Vernia", fractions1);
     }
@@ -34,40 +37,41 @@ class FractionTest {
 
     @Test
     void findAllFractionsDecimal() {
-        assertEquals(3.0, user.getFractions().get(2).decimal());
+        assertEquals(2.0, user.getFractions().get(2).decimal());
     }
 
     @Test
-    void findIfAreProperFunctions(){
+    void findIfAreProperFunctions() {
         assertEquals(true, user.getFractions().get(0).isProper());
         assertEquals(false, user.getFractions().get(1).isProper());
     }
 
     @Test
-    void findIfAreImproperFunctions(){
+    void findIfAreImproperFunctions() {
         assertEquals(false, user.getFractions().get(0).isImproper());
         assertEquals(true, user.getFractions().get(1).isImproper());
     }
 
     @Test
-    void findMultiply(){
+    void findMultiply() {
         assertEquals(new Fraction(0, -12), user.getFractions().get(0).multiply(user.getFractions().get(1)));
     }
 
     @Test
-    void findDivide(){
+    void findDivide() {
         assertEquals(new Fraction(0, 4), user.getFractions().get(0).divide(user.getFractions().get(1)));
     }
 
     @Test
     void findIfAreEquivalentFunctions() {
         assertEquals(false, user.getFractions().get(0).isEquivalent(user.getFractions().get(1)));
-        assertEquals(false, user.getFractions().get(0).isEquivalent(user.getFractions().get(2)));
+        assertEquals(true, user.getFractions().get(3).isEquivalent(user.getFractions().get(2)));
     }
 
     @Test
-    void findFractionsSum(){
+    void findFractionsSum() {
         assertEquals(new Fraction(4, -12), user.getFractions().get(0).add(user.getFractions().get(1)));
-        assertEquals(new Fraction(-8, -3), user.getFractions().get(2).add(user.getFractions().get(1)));
+        assertEquals(new Fraction(11, 2), user.getFractions().get(2).add(user.getFractions().get(4)));
     }
 }
+
