dc07cc50f51ff5934076fc23904c13dadccfd80c
Mariavernia <maversa2@alumni.uv.es>
2021-10-01 10:55:36 +0200

    add user test #7

--- src/test/java/es/upm/miw/iwvg_devops/rest/code/UserTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/code/UserTest.java
@@ -1,5 +1,8 @@
 package es.upm.miw.iwvg_devops.rest.code;
 
+import es.upm.miw.iwvg_devops.code.Fraction;
+import es.upm.miw.iwvg_devops.code.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,7 +12,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class UserTest {
 
     @Test
-    void findAllUsers() {
+    void findAllUsersId() {
+        assertEquals(null, new User().getId());
+    }
 
+    @Test
+    void findAllUsersName() {
+        assertEquals(null, new User().getName());
+    }
+
+    @Test
+    void findUsersFamilyName() {
+        assertEquals(null, new User().getFamilyName());
+    }
+
+    @Test
+    void findAllUsersFractions() {
+        assertEquals(List.of( ), new User().getFractions());
+    }
+
+    @Test
+    void findAllUsersFullName() {
+        assertEquals("null null" , new User().fullName());
     }
 }
